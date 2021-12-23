46d9ddf599bef4542546fe397bb21c5fedb069ea
Arun Solanki <96058824+Arun-solanki-301@users.noreply.github.com>
2021-12-23 16:09:16 +0530

    Add files via upload

--- formFunctions/LoginFunction.js
+++ formFunctions/LoginFunction.js
@@ -1,23 +1,33 @@
-export function FormValidation(UserEmail, UserPassword) {
-  if (UserEmail.length === 0 && UserPassword.length === 0) {
-    return false;
-  }
-  else {
-    return true;
-  }
+
+export const emailLengthValidation = (userEmail)=>{
+  // console.log(userEmail.length)
+  if(userEmail.length===0) return false
+  else return true ;
 }
-
-export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
-
-
-export function emailValidation(email) {
+export const emailOtherValidation = (userEmail)=> {
   let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-  if (reg.test(email) === false) {
+  
+  if (reg.test(userEmail) === false) {
     return false;
   } else {
     return true;
   }
 }
+export const passwordLengthValidation = (userPassword)=>{
+  // console.log(userPassword)
+  if(userPassword.length===0) return false;
+  else return true;
+}
+// export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+export const validPassword=(userPassword)=>{
+  const reg = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$')
+  if (reg.test(userPassword) === false) {
+    return false;
+  } else {
+    return true;
+  }
 
-// export {FormValdation};
-// export {PasswordValidate};
+}
+
+
+
